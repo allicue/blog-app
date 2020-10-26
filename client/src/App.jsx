d02612d9bb4fd6c0e8d1c33a1da851490f9bc698
Daniel Michael <itzdanielmichael@gmail.com>
2020-10-26 15:46:56 -0400

    fixed warning in postcards.jsx (no alt value for img), added PostDetail as route and component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Home from './screens/Home/Home'
 import CreatePost from './screens/CreatePost/CreatePost'
+import PostDetail from './screens/PostDetail/PostDetail'
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/CreatePost' component={CreatePost} />
+        <Route exact path='/posts/:id' component={PostDetail}/>
       </Switch>
 
     </div>
