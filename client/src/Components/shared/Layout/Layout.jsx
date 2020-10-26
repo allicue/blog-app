fbc1e2e6ee70383194864af659f9a45559f3c959
Daniel Michael <itzdanielmichael@gmail.com>
2020-10-26 13:59:30 -0400

    created basic components, (Layout) (Home.jsx) and setup routes in app.jsx

---
+++ client/src/Components/shared/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
+
+function Layout(props) {
+  return (
+    <div>
+      <Nav />
+      <div className="layout-children">
+        {props.children}
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Layout from '../../Components/shared/Layout/Layout'
+import React from 'react'
+
+function Home() {
+  return (
+    <Layout>
+  <div className="home">
+      <h1>Home Page</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
