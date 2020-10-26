e95464a92b070e61c25e8ec37b8a31bf7edcf799
Allison <allisonquiroz89@gmail.com>
2020-10-26 15:16:16 -0400

    Laid out post renderings on homepage

---
+++ client/src/Components/PostCards/PostCards.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './PostCards.css';
+import PostCard from '../PostCard/PostCard'
+import { getPosts } from '../../services/posts'
+
+class PostCards extends Component {
+  constructor() {
+    super()
+    this.state = {
+      posts: []
+    }
+  }
+
+  async componentDidMount() {
+    const posts = await getPosts()
+    this.setState({ posts })
+  }
+
+  render() {
+
+    const CARDS = this.state.posts.map((post, index) =>
+      <PostCard _id={post._id} name={post.name} imgURL={post.imgURL} key={index} />
+    )
+
+    return (
+      <div className="post-cards">
+        <div className="cards">
+          {CARDS}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PostCards
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,11 +1,13 @@
-import Layout from '../../Components/shared/Layout/Layout'
-import React from 'react'
+import Layout from '../../Components/shared/Layout/Layout';
+import React from 'react';
+import PostCards from '../../Components/PostCards/PostCards'
 
 function Home() {
   return (
     <Layout>
   <div className="home">
-      <h1>Home Page</h1>
+        <h1>Home Page</h1>
+        <PostCards/>
       </div>
     </Layout>
   )
